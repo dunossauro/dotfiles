03abc397ea88535de5c6f1cc32f7ba541c5cb278
dunossauro <mendesxeduardo@gmail.com>
2023-09-13 18:35:47 -0300

    fonte e geometria da janela no emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,14 +9,18 @@
 (scroll-bar-mode -1)
 (setq-default message-log-max nil)
 (kill-buffer "*Messages*")
-(kill-buffer "*scratch*")
+
+;; window size
+(add-to-list 'default-frame-alist '(height . 26))
+(add-to-list 'default-frame-alist '(width . 89))
 
 ;; modes
 (ido-mode 1)
 (cua-mode 1)
+(display-time-mode 1)
 
 ;; Font size
-(set-face-attribute 'default nil :font "DejaVu Sans Mono Book" :height 150)
+(set-face-attribute 'default nil :font "DejaVu Sans Mono Book" :height 220)
 
 ;; flex buffer
 (defalias 'list-buffers 'ibuffer-other-window) ;; ibuffer default C-x C-b
