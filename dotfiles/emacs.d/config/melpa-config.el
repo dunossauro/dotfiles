07e78ddbbdac234c034a52dfe41bc5ad106737bd
dunossauro <mendesxeduardo@gmail.com>
2026-03-15 12:42:57 -0300

    usando https para elpa e melpa

--- dotfiles/emacs.d/config/melpa-config.el
+++ dotfiles/emacs.d/config/melpa-config.el
@@ -1,8 +1,8 @@
 (require 'package)
 
 (setq package-enable-at-startup nil)
-(setq package-archives '(("elpa" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "http://melpa.org/packages/")
+(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")))
 
 (package-initialize)
